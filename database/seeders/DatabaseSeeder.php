e31d8c862d4ac03f22e51acc6266539cfcbbfcf3
Haqi25 <baihaqi1401@student.ub.ac.id>
2026-02-16 03:05:48 +0700

    Add DriversSeeder to seed drivers in the database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use SebastianBergmann\CodeCoverage\Driver\Driver;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,8 +18,8 @@ class DatabaseSeeder extends Seeder
     {
        $this->call([
         RoleSeeder::class,
-        UserSeeder::class
-
+        UserSeeder::class,
+        DriversSeeder::class
        ]);
     }
 }
