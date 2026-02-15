03f9f42e002d94e72d9eb5e302657b8a8e4e43bb
Haqi25 <baihaqi1401@student.ub.ac.id>
2026-02-16 02:40:35 +0700

    Add RoleSeeder to seed roles in the database

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Carbon;
+
+use function Symfony\Component\String\s;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+         DB::table('roles')->insert([
+            [
+            'role_name' => 'ADMIN', 
+            'description' => 'Admin Travel',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now() 
+            ],
+          
+         [
+            'role_name' => 'USER',
+            'description' => 'Pelanggan Travel',
+             'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+ 
+         ], [
+            'role_name' => 'DRIVER',
+            'description' => 'Sopir Travel',
+             'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+         ]
+         ]);
+    }
+}
