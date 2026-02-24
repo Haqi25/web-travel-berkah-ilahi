a08deb9955f4e998aad4b2b5af31d3196cdec4a7
Haqi25 <baihaqi1401@student.ub.ac.id>
2026-02-25 05:23:07 +0700

    Add multiple schedule entries to SchedulesSeeder and include available seat information; add mobil.jpeg image file

--- database/seeders/SchedulesSeeder.php
+++ database/seeders/SchedulesSeeder.php
@@ -17,15 +17,82 @@ class SchedulesSeeder extends Seeder
     public function run(): void
     {
         DB::table('schedules')->insertOrIgnore([
-            
+
+            [
                 'route_id' => 1,
                 'driver_id' => 1,
                 'vehicle_id' => 3,
                 'departure_time' => Carbon::now()->addDay(),
                 'available_seat' => 6,
-                'created_at'=> Carbon::now(),
+                'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
-            
-            ]);
+            ],
+            [
+                'route_id' => 2,
+                'driver_id' => 2,
+                'vehicle_id' => 2,
+                'departure_time' => Carbon::now()->addDay(),
+                'available_seat' => 6,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'route_id' => 3,
+                'driver_id' => 3,
+                'vehicle_id' => 4,
+                'departure_time' => Carbon::now()->addDay(),
+                'available_seat' => 6,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+
+            [
+                'route_id' => 4,
+                'driver_id' => 4,
+                'vehicle_id' => 4,
+                'departure_time' => Carbon::now()->addDay(),
+                'available_seat' => 6,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'route_id' => 5,
+                'driver_id' => 5,
+                'vehicle_id' => 5,
+                'departure_time' => Carbon::now()->addDay(),
+                'available_seat' => 6,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'route_id' => 6,
+                'driver_id' => 6,
+                'vehicle_id' => 6,
+                'departure_time' => Carbon::now()->addDay(),
+                'available_seat' => 6,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'route_id' => 7,
+                'driver_id' => 2,
+                'vehicle_id' => 7,
+                'departure_time' => Carbon::now()->addDay(),
+                'available_seat' => 6,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'route_id' => 8,
+                'driver_id' => 4,
+                'vehicle_id' => 6,
+                'departure_time' => Carbon::now()->addDay(),
+                'available_seat' => 6,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+
+            ]
+
+        ]);
     }
 }
