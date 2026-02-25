1b059cac01c5e63b00570dc5b8f26e74f0ac7c4b
Haqi25 <baihaqi1401@student.ub.ac.id>
2026-02-26 01:27:50 +0700

    Fix route name for home page and ensure schedules route is correctly defined

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\guest\homeController;
+use App\Http\Controllers\guest\scheduleController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [homeController::class, 'index'])->name('schedules');
+Route::get('/', [homeController::class, 'index'])->name('home');
+Route::get('/schedules', [scheduleController::class, 'index'])->name('schedules');
