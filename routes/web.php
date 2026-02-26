c70290e70ae3b793138be1e3d0ef7bed40695e89
Haqi25 <baihaqi1401@student.ub.ac.id>
2026-02-27 04:59:00 +0700

    Add checkout and invoice routes: Define routes for checkout and invoice views in web.php

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [homeController::class, 'index'])->name('home');
 Route::get('/schedules', [scheduleController::class, 'index'])->name('schedules');
+Route::get('/checkout', function(){
+    return view('guest.checkout');
+});
+
+Route::get('/invoice', function(){
+    return view('guest.invoice');
+});
