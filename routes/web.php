602e66105276e8e9fa0ea5896ae0ff987f33647c
Haqi25 <baihaqi1401@student.ub.ac.id>
2026-03-01 04:11:27 +0700

    Refactor checkout routes: Update checkout route to include ID parameter and remove unused invoice route

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,16 @@
 <?php
 
 use App\Http\Controllers\guest\homeController;
+use App\Http\Controllers\guest\paymentController;
 use App\Http\Controllers\guest\scheduleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [homeController::class, 'index'])->name('home');
 Route::get('/schedules', [scheduleController::class, 'index'])->name('schedules');
-Route::get('/checkout', function(){
-    return view('guest.checkout');
-});
+Route::get('/checkout/{id}', [paymentController::class, 'checkout'])->name('checkout');
 
-Route::get('/invoice', function(){
-    return view('guest.invoice');
-});
+Route::post('/payment',  [paymentController::class, 'storeOrder'])->name('payment');
+
+Route::post('/payments/webhook', [paymentController::class, 'handleCallback']);
+
+Route::get('/success/{orderId}', [paymentController::class, 'checkoutSuccess']);
