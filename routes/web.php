704f0d626b9edbc5ed2bd966e9de52dd23aea91e
Haqi25 <baihaqi1401@student.ub.ac.id>
2026-03-17 17:44:18 +0700

    Refactor routes to use closures for schedule and checkout pages

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,21 @@ use App\Http\Controllers\guest\paymentController;
 use App\Http\Controllers\guest\scheduleController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [homeController::class, 'index'])->name('home');
-Route::get('/schedules', [scheduleController::class, 'index'])->name('schedules');
-Route::get('/checkout/{id}', [paymentController::class, 'checkout'])->name('checkout');
+// Route::get('/', [homeController::class, 'index'])->name('home');
+// Route::get('/schedules', [scheduleController::class, 'index'])->name('schedules');
+Route::get('/schedules', function(){
+    return view('user.schedule');
+}); 
+// Route::get('/checkout/{id}', [paymentController::class, 'checkout'])->name('checkout');
 
+Route::get('/checkout', function(){
+    return view('user.checkout');
+});
 Route::post('/payment',  [paymentController::class, 'storeOrder'])->name('payment');
 
 Route::post('/payments/webhook', [paymentController::class, 'handleCallback']);
 
 Route::get('/success/{orderId}', [paymentController::class, 'checkoutSuccess']);
+Route::get('/', function (){
+    return view('user.landingPage');
+});
