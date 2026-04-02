b684e6df313f1277d4ed034e2b2c743c2908ade1
Haqi25 <baihaqi1401@student.ub.ac.id>
2026-04-02 20:00:43 +0700

    Restore original route definitions for schedules and home; remove commented-out code for clarity

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,9 @@ use App\Http\Controllers\guest\scheduleController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', [homeController::class, 'index'])->name('home');
-// Route::get('/schedules', [scheduleController::class, 'index'])->name('schedules');
-Route::get('/schedules', function(){
-    return view('user.schedule');
-}); 
-// Route::get('/checkout/{id}', [paymentController::class, 'checkout'])->name('checkout');
+Route::get('/schedules', [scheduleController::class, 'index'])->name('schedules');
+
+Route::get('/checkout/{id}', [paymentController::class, 'checkout'])->name('checkout');
 
 Route::get('/checkout', function(){
     return view('user.checkout');
@@ -20,6 +18,4 @@ Route::post('/payment',  [paymentController::class, 'storeOrder'])->name('paymen
 Route::post('/payments/webhook', [paymentController::class, 'handleCallback']);
 
 Route::get('/success/{orderId}', [paymentController::class, 'checkoutSuccess']);
-Route::get('/', function (){
-    return view('user.landingPage');
-});
+Route::get('/', [homeController::class, 'index'])->name('home');
