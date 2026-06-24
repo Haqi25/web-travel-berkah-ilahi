ee4a4db25ec0e889cf3f5c5076491f53272d5b65
Haqi25 <baihaqi1401@student.ub.ac.id>
2026-06-24 18:59:46 +0700

    fix: add missing Carbon import in AdminSeeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 class AdminSeeder extends Seeder
 {
     /**
